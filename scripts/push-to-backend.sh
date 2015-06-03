c1f776f2d6cdeab09de30f29957985b1d3e388c6
lovedota <ngocdung_dng@yahoo.com>
2015-06-03 14:02:26 +0700

    test

--- scripts/push-to-backend.sh
+++ scripts/push-to-backend.sh
@@ -2,17 +2,14 @@
 
 rm -rf out || exit 0;
 mkdir out;
-gulp build
 
 (
 cd out
-git init
+git clone "https://github.com/lovedota/angular-regex-expression.git"
 git config user.name "Travis-CI"
 git config user.email "travis@example.com"
-cp ../CNAME .
-cp ../index.html .
 cp -rf ../dist .
 git add .
 git commit -m "Deployed to Public Folder"
-git push --force --quiet "https://github.com/lovedota/angular-regex-expression.git" master > /dev/null 2>&1
+git push --force --quiet master > /dev/null 2>&1
 )
