a7a3b83ce0d0a6669b0fbf766a35ed91ab88f051
lovedota <ngocdung_dng@yahoo.com>
2015-06-03 14:53:18 +0700

    test test

--- scripts/push-to-backend.sh
+++ scripts/push-to-backend.sh
@@ -10,8 +10,17 @@ git init
 git config user.name "Travis-CI"
 git config user.email "travis@example.com"
 
+
+#using token clone gh-pages branch
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lovedota/angular-regex-expression.git  gh-pages > /dev/null
+
+#go into diractory and copy data we're interested in to that directory
+cd gh-pages
 cp -rf ../dist .
 
-git add .
-git commit -m "Deployed to Public Folder"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Done magic with coverage\n"
